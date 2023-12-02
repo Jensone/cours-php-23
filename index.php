3faae16c0de2be8fd6549180b8032ba46ce1e19d
Jensone <hello@jensone.com>
2023-12-02 22:49:08 +0100

    Refactor index.php to display posts and comments

--- index.php
+++ index.php
@@ -1,36 +1,22 @@
-<!DOCTYPE html>
-<html lang="fr">
+<?php include 'partials/header.php'; 
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Form in PHP</title>
-    <link rel="icon" href="https://api.iconify.design/logos:php-alt.svg" type="image/svg+xml">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css">
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;400;600&display=swap" rel="stylesheet">
-</head>
+$data = file_get_contents('data.json'); // Fetch du fichier JSON
+$data = json_decode($data, true); // Convertion du JSON en tableau associatif
 
-<body style="font-family: 'Inter', sans-serif;">
-    <div class="container" style="padding:1em; text-align:center;">
-        <kbd>PHP STARTER</kbd>
-    </div>
-    <main class="container">
-        <form action="signup.php" method="post">
-            <fieldset>
-                <legend>Inscription</legend>
-                <input type="text" name="name" placeholder="Martin" required>
-            </fieldset>
-            <fieldset>
-                <legend>Votre âge</legend>
-                <input type="number" name="age" placeholder="32" required>
-            </fieldset>
-            <fieldset>
-                <legend>Votre 1er dépôt</legend>
-                <input type="number" name="amount" placeholder="1900.50" required>
-            </fieldset>
-            <input type="submit" value="S'inscrire">
-        </form>
-    </main>
-</body>
+$users = $data['users']; // Assignation des données "users"
+$posts = $data['posts']; // Assignation des données "posts"
 
-</html>
+// Boucle sur les posts
+foreach ($posts as $item) {
+    // Assignation des variables
+    $title = $item['title'];
+    $content = $item['content'];
+    $author = $users[$item['user_id']]['username'];
+    $commentCount = count($item['comments']);
+
+    // Inclusion du template
+    include 'partials/post.php';
+    echo '<hr/>';
+}
+
+include 'partials/footer.php'; ?>
